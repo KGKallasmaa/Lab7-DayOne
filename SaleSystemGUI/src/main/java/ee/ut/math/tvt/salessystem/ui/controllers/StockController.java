f9c02e7e47b6ecadd1f07888bf79d6f22b394ab1
EZEQUIEL-PC\Ezequiel <ezequielscott@gmail.com>
2017-06-27 15:21:07 +0300

    Initial project upload

---
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/StockController.java
@@ -0,0 +1,42 @@
+package ee.ut.math.tvt.salessystem.ui.controllers;
+
+import com.sun.javafx.collections.ObservableListWrapper;
+import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
+import ee.ut.math.tvt.salessystem.dataobjects.StockItem;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StockController implements Initializable {
+
+    private final SalesSystemDAO dao;
+
+    @FXML
+    private Button addItem;
+    @FXML
+    private TableView<StockItem> warehouseTableView;
+
+    public StockController(SalesSystemDAO dao) {
+        this.dao = dao;
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        refreshStockItems();
+        // TODO refresh view after adding new items
+    }
+
+    @FXML
+    public void refreshButtonClicked() {
+        refreshStockItems();
+    }
+
+    private void refreshStockItems() {
+        warehouseTableView.setItems(new ObservableListWrapper<>(dao.findStockItems()));
+        warehouseTableView.refresh();
+    }
+}
