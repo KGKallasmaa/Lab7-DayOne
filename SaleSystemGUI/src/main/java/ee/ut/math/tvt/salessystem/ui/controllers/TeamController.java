59106a58ab57fbaef88ba16c99009c9d209db287
KGKallasmaa <karl.gustav1789@gmail.com>
2017-10-20 10:57:59 +0300

    application.properties devlopment
    see #32

--- SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamController.java
+++ SaleSystemGUI/src/main/java/ee/ut/math/tvt/salessystem/ui/controllers/TeamController.java
@@ -1,51 +1,102 @@
 package ee.ut.math.tvt.salessystem.ui.controllers;
 
-import ee.ut.math.tvt.salessystem.dao.SalesSystemDAO;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 
+import javax.swing.text.html.ImageView;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
+import java.util.Properties;
 import java.util.ResourceBundle;
 
-public class TeamController implements Initializable  {
+public class TeamController implements Initializable {
 
-    @FXML private Text team_name;
-    @FXML private Text team_leader;
-    @FXML private Text team_leader_email;
-    @FXML private Text team_members;
+    @FXML
+    private Text team_name;
+    @FXML
+    private Text team_leader;
+    @FXML
+    private Text team_leader_email;
+    @FXML
+    private Text team_members;
 
-    public TeamController(){
+    public TeamController() {
     }
 
-    public Text getTeam_name(){
+    public Text getTeam_name() {
         return team_name;
     }
-    public Text getTeam_leader(){
+
+    public Text getTeam_leader() {
         return team_leader;
     }
-    public Text getTeam_leader_email(){
+
+    public Text getTeam_leader_email() {
         return team_leader_email;
     }
-    public Text getTeam_members(){
+
+    public Text getTeam_members() {
         return team_members;
     }
-    protected void setTeam_name(Text new_name){
-        this.team_name = new_name;
-    }
-    protected void setTeam_leader(Text new_leader){
-        this.team_leader = new_leader;
-    }
-    protected void setTeam_leader_email(Text new_email){
-        this.team_leader_email = new_email;
-    }
-    protected void setTeam_members(Text new_members){
-        this.team_members = new_members;
+
+    protected void setTeam_name(String text) {
+        Text new_text = new Text(text);
+        this.team_name = new_text;
     }
 
+    protected void setTeam_leader(String text) {
+        Text new_text = new Text(text);
+        this.team_leader = new_text;
+    }
+
+    protected void setTeam_leader_email(String text) {
+        Text new_text = new Text(text);
+        this.team_leader_email = new_text;
+    }
+
+    protected void setTeam_members(String text) {
+        Text new_text = new Text(text);
+        this.team_members = new_text;
+    }
+
+
+    public Properties load_prop (){
+        Properties prop = new Properties();
+        InputStream input = null;
+        try {
+            String propFileName = "application.properties";
+            input = TeamController.class.getClassLoader().getResourceAsStream(propFileName);
+            if(input != null){
+                prop.load(input);
+            }
+        } catch (IOException ex) {
+            String msg;
+            msg = "Error accessing properties file";
+            System.out.println(msg);
+        } finally {
+            if (input != null) {
+                try {
+                    input.close();
+                    return  prop;
+                } catch (IOException e) {
+                    e.printStackTrace();
+
+                }
+            }
+        }
+        return prop;
+    }
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
+        Properties prop = load_prop();
+        System.out.println(prop);
+        //BROKEN~!!!
+        this.team_name.setText(prop.getProperty("team_name"));
+        this.team_leader.setText(prop.getProperty("team_leader"));
+        this.team_leader_email.setText(prop.getProperty("team_leader_email"));
+        this.team_members.setText(prop.getProperty("team_members"));
     }
 }
